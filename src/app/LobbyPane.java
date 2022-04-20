cfc165b02150b43eb4b8b1debb30d13035812d9f
Bingzhe Jin <jbzjbz@126.com>
2022-04-20 13:57:31 +1000

    Lobby

--- src/app/LobbyPane.java
+++ src/app/LobbyPane.java
@@ -3,7 +3,11 @@ package app;
 import javax.swing.*;
 
 public class LobbyPane extends JPanel {
-    JList<Room> roomList;
+    private JList<Room> roomList;
+    private JButton createRoom;
+    private JButton joinRoom;
+    private JTextField searchBar;
+    private JButton searchButton;
 
     public LobbyPane(Room[] rooms) {
         super();
@@ -11,13 +15,35 @@ public class LobbyPane extends JPanel {
         roomList = new JList<Room>(rooms);
         roomList.setCellRenderer(new RoomRender());
 
-        JScrollPane sp = new JScrollPane(roomList, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        JScrollPane sp = new JScrollPane(roomList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
         //scroll speed
         sp.getVerticalScrollBar().setUnitIncrement(10);
 
-        sp.setBounds(300, 200, 600, 400);
-        this.add(sp);
+        sp.setBounds(300, 100, 600, 600);
 
+
+        createRoom = new JButton("Create Room");
+        createRoom.setBounds(350, 720, 150, 30);
+
+
+        joinRoom = new JButton("Join Room");
+        joinRoom.addActionListener(e -> {
+            System.out.println(roomList.getSelectedIndex());
+            System.out.println(roomList.getSelectedValue());
+        });
+        joinRoom.setBounds(700, 720, 150, 30);
+
+        searchBar = new JTextField();
+        searchBar.setBounds(350, 50, 300, 40);
+
+        searchButton = new JButton("Search");
+        searchButton.setBounds(655, 55, 100, 30);
+
+        this.add(sp);
+        this.add(createRoom);
+        this.add(joinRoom);
+        this.add(searchBar);
+        this.add(searchButton);
     }
 }
--- src/app/RoomRender.java
+++ src/app/RoomRender.java
@@ -1,6 +1,7 @@
 package app;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class RoomRender extends JPanel implements ListCellRenderer<Room> {
@@ -8,14 +9,18 @@ public class RoomRender extends JPanel implements ListCellRenderer<Room> {
     @Override
     public Component getListCellRendererComponent(JList<? extends Room> list, Room value, int index, boolean isSelected, boolean cellHasFocus) {
         this.removeAll();
+        this.setLayout(null);
         this.setBackground(Color.gray);
 
         JLabel name = new JLabel(value.roomName);
-        name.setBounds(50, 50, 200, 100);
+        name.setBounds(30, 0, 200, 100);
         this.add(name);
 
-        JLabel num = new JLabel(value.numPlayer + "/" + value.maxPlayer);
-
+        JLabel num = new JLabel(value.numPlayer + "/" + value.maxPlayer, SwingConstants.RIGHT);
+        num.setBounds(400, 0, 100, 100);
+        this.add(num);
+        Border blackLine = BorderFactory.createLineBorder(Color.black);
+        this.setBorder(blackLine);
         if (isSelected) {
             this.setBackground(Color.blue);
         }
@@ -26,6 +31,6 @@ public class RoomRender extends JPanel implements ListCellRenderer<Room> {
 
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(600, 200);
+        return new Dimension(600, 100);
     }
 }
